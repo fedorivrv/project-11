172c98549fdea207e743705733cc68442be6d726
Vasyl Linkin <linkinmvv@gmail.com>
2025-08-30 12:04:46 +0300

    add if isMessage, timeout

--- src/js/izitoast-functions.js
+++ src/js/izitoast-functions.js
@@ -2,32 +2,41 @@ import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
 export function noDataIzT(message) {
-  iziToast.show({
-    title: '❌',
-    message: `Sorry, there are no ${message}.`,
-    color: 'red',
-    position: 'topRight',
-    messageColor: 'white',
-    titleColor: 'white',
-  });
+  if (message) {
+    iziToast.show({
+      title: '❌',
+      message: `Sorry, there are no ${message}.`,
+      color: 'red',
+      position: 'topRight',
+      messageColor: 'white',
+      titleColor: 'white',
+      timeout: 5000,
+    });
+  }
 }
 
 export function errorApiIzT(error) {
-  iziToast.show({
-    title: 'Error',
-    color: 'red',
-    position: 'topRight',
-    messageColor: 'white',
-    titleColor: 'white',
-    message: error.message,
-  });
+  if (error.message) {
+    iziToast.show({
+      title: 'Error',
+      color: 'red',
+      position: 'topRight',
+      messageColor: 'white',
+      titleColor: 'white',
+      message: error.message,
+      timeout: 5000,
+    });
+  }
 }
 
 export function successDataIzT(response) {
-  iziToast.success({
-    message: response.data.message,
-    position: 'topRight',
-    messageColor: 'white',
-    titleColor: 'white',
-  });
+  if (response.data.message) {
+    iziToast.success({
+      message: response.data.message,
+      position: 'topRight',
+      messageColor: 'white',
+      titleColor: 'white',
+      timeout: 5000,
+    });
+  }
 }
